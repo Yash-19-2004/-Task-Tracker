0f733e48572c397381d2dfda315727c3b6a16c8a
MandeepJas <4nm20is077@nmamit.in>
2026-06-28 17:20:05 +0530

    Fix production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://name-task-tracker-api.onrender.com",
+  baseURL: "https://name-task-tracker-api.onrender.com/api",
 });
 
 export default api;
